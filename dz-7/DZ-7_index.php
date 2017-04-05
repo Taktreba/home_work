04def30e31d0c5630681d4106fbac4641420d896
Taktreba <leo_motor@mail.ru>
2017-04-05 13:54:25 +0300

    fix bag

--- dz-7/DZ-7_index.php
+++ dz-7/DZ-7_index.php
@@ -1,33 +1,37 @@
 <?php
-if (isset($_GET['submit'])) {
-    $firs_name = trim(strip_tags($_GET['firs_name'])); // собираем переменные из GET
-    $last_name = trim(strip_tags($_GET['last_name']));
-    $phone = trim(strip_tags($_GET['phone']));
-    $age = trim(strip_tags($_GET['age']));
-    $arr = [$firs_name, $last_name, $phone, $age]; // Запихиваем их в массив для сериализации
-    $codeArr = serialize($arr);
-    setcookie('data', $codeArr, time() + 60 * 60 * 3); // кука установлена на 3 часа
-} elseif (isset($_COOKIE['data'])) { // если кука установлена мы ее записываем в имя и возраст что бы не потерять значения когда выйдем с страницы
-    $unSer = unserialize($_COOKIE['data']);
-    $firs_name = $unSer[0];
-    $age = $unSer[3];
-
-    $bd = explode('-', $unSer[3]);// разбиваем дату рождения на массив
-    if ($bd[1] > date('m') || $bd[1] == date('m') && $bd[2] > date('d')) {
-        $bd_day = round((mktime(23, 59, 59, $bd[1], $bd[2]) - time()) / (60 * 60 * 24)); // что то не работает нужно по вводить разные даты
-        echo 'до дня рождения ' . $bd_day . ' дней';
-    } elseif ($bd[1] < date('m') || $bd[1] == date('m') && $bd[2] < date('d')) {
-        $bd_day = round((mktime(23, 59, 59, $bd[1], $bd[2], date('Y') + 1) - time()) / (60 * 60 * 24));
-        echo 'до дня рождения ' . $bd_day . ' дней';
-    }elseif ($bd[1] == date('m') || $bd[1] == date('d')) {
-        echo 'У Вас сегодня день рождения и мы Вас с этим торжественно поздравляем!';
+if (!empty($_GET['firs_name']) && !empty($_GET['last_name']) && !empty($_GET['phone']) && !empty($_GET['age'])) {
+    if (isset($_GET['submit'])) {
+        $firs_name = trim(strip_tags($_GET['firs_name'])); // собираем переменные из GET
+        $last_name = trim(strip_tags($_GET['last_name']));
+        $phone = trim(strip_tags($_GET['phone']));
+        $age = trim(strip_tags($_GET['age']));
+        $arr = [$firs_name, $last_name, $phone, $age]; // Запихиваем их в массив для сериализации
+        $codeArr = serialize($arr);
+        setcookie('data', $codeArr, time() + 60 * 60 * 3); // кука установлена на 3 часа
     }
-
-
 } else {
-    $firs_name = 'Гость';
-    $age = 'извините - возраст еще не определен';
+    if (isset($_COOKIE['data'])) { // если кука установлена мы ее записываем в имя и возраст что бы не потерять значения когда выйдем с страницы
+        $unSer = unserialize($_COOKIE['data']);
+        $firs_name = $unSer[0];
+        $age = $unSer[3];
+
+        $bd = explode('-', $unSer[3]);// разбиваем дату рождения на массив
+        if ($bd[1] > date('m') || $bd[1] == date('m') && $bd[2] > date('d')) {
+            $bd_day = round((mktime(23, 59, 59, $bd[1], $bd[2]) - time()) / (60 * 60 * 24)); // что то не работает нужно по вводить разные даты
+            echo 'до дня рождения ' . $bd_day . ' дней';
+        } elseif ($bd[1] < date('m') || $bd[1] == date('m') && $bd[2] < date('d')) {
+            $bd_day = round((mktime(23, 59, 59, $bd[1], $bd[2], date('Y') + 1) - time()) / (60 * 60 * 24));
+            echo 'до дня рождения ' . $bd_day . ' дней';
+        } elseif ($bd[1] == date('m') || $bd[1] == date('d')) {
+            echo 'У Вас сегодня день рождения и мы Вас с этим торжественно поздравляем!';
+        }
+    } else {
+        $firs_name = 'Гость';
+        $age = 'извините - возраст еще не определен';
+        echo 'Введите все данные в форму отправки';
+    }
 }
+
 
 //
 //$q = explode('-', $age);
